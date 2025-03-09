582847e59f091b60bd7d43a71eda475166fa0b54
Mohit22735 <mkpcpc28@gmail.com>
2025-03-09 23:14:47 +0530

    Initial commit

--- src/app/Components/nav/page.js
+++ src/app/Components/nav/page.js
@@ -9,15 +9,13 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full top-0 left-0 z-50   bg-gradient-to-r from-green-500 to-green-800  shadow-lg">
+    <nav className="fixed w-full top-0 left-0 z-50 bg-gradient-to-r from-green-500 to-green-800 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo / Title */}
           <div className="text-2xl font-bold text-white flex items-center">
             <LuBrainCircuit className="bg-red-500 p-1" />
-            
-            <NavLink href="/" label=" Brain Workout"  />
-
+            <NavLink href="/" label="Brain Workout" onClick={() => setIsOpen(false)} />
           </div>
 
           {/* Desktop Menu */}
@@ -41,11 +39,11 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden  bg-gradient-to-r from-green-500 to-green-800 shadow-md py-4">
+        <div className="md:hidden bg-gradient-to-r from-green-500 to-green-800 shadow-md py-4">
           <div className="flex flex-col space-y-2 px-4">
-            <NavLink href="/" label="Home" icon={<FaHome />} />
-            <NavLink href="/buy-me-a-coffee" label="Buy Me a Coffee" icon={<FaCoffee />} />
-            <NavLink href="mailto:mkpcpc28@gmail.com" label="mkpcpc28@gmail.com" icon={<FaEnvelope />} />
+            <NavLink href="/" label="Home" icon={<FaHome />} onClick={() => setIsOpen(false)} />
+            <NavLink href="/buy-me-a-coffee" label="Buy Me a Coffee" icon={<FaCoffee />} onClick={() => setIsOpen(false)} />
+            <NavLink href="mailto:mkpcpc28@gmail.com" label="mkpcpc28@gmail.com" icon={<FaEnvelope />} onClick={() => setIsOpen(false)} />
           </div>
         </div>
       )}
@@ -53,10 +51,11 @@ const Navbar = () => {
   );
 };
 
-const NavLink = ({ href, label, icon }) => (
+const NavLink = ({ href, label, icon, onClick }) => (
   <Link
     href={href}
     className="flex items-center space-x-2 text-white hover:text-blue-200 transition-colors duration-300"
+    onClick={onClick}
   >
     {icon} <span>{label}</span>
   </Link>
